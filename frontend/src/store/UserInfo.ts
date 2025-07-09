4e350cf520cbbe82943c34a7e2af23c3b69b29fc
Alim <abdulalimkhan2794@gmail.com>
2025-07-09 11:39:46 -0400

    Added MobX store to store user info

---
+++ frontend/src/store/UserInfo.ts
@@ -0,0 +1,28 @@
+import { makeAutoObservable } from "mobx";
+
+export interface User {
+    name : string
+    email : string
+}
+
+class UserInfo {
+    user : User |null = {name : "alim", email: "alim@dummy.com"}
+    constructor(){
+        makeAutoObservable(this)
+    }
+
+    setUserInfo(user: User){
+        this.user = user
+    }
+
+    clearUserInfo() {
+        this.user = null
+    }
+
+    get isLoggedIn(): boolean{
+        return this.user !== null
+    }
+}
+
+const userStore = new UserInfo();
+export default userStore
